d800b0e70b154f573a77897927bccc889f8c83d7
Greg Guthe <greg.guthe@gmail.com>
2014-06-04 23:36:36 -0400

    Resizable SVG with goog events

--- src/circle_editor/core.cljs
+++ src/circle_editor/core.cljs
@@ -1,16 +1,27 @@
 (ns circle-editor.core
-  (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+  (:require
+   [goog.events :as events]
+   [om.core :as om :include-macros true]
+   [om.dom :as dom :include-macros true])
+  (:import [goog.events EventType]))
 
 (enable-console-print!)
 
-(def app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:text "Hello world!"
+                      :height (.-innerHeight js/window)
+                      :width (.-innerWidth js/window)
+                      }))
+
+(defn update-svg-size [_]
+  (swap! app-state assoc :height (.-innerHeight js/window))
+  (swap! app-state assoc :width (.-innerWidth js/window)))
+
+(events/listen js/window EventType/RESIZE update-svg-size)
+
 
 (om/root
-  (fn [app owner]
-    (let [height (.-innerHeight js/window)
-          width (.-innerWidth js/window)
-          ]
+ (fn [{:keys [height width] :as app} owner]
+    (let []
       (dom/div nil
                (dom/svg #js {:height height
                              :width width
